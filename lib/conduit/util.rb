36d305f1c882856a1b785a7ecfb9bf6378fad01d
Beardo <jason.moore@hellolabs.com>
2015-04-28 12:59:46 -0600

    Add ! version of find_driver that throws exception

--- lib/conduit/util.rb
+++ lib/conduit/util.rb
@@ -7,6 +7,11 @@ module Conduit
     # => Conduit::Driver::Fusion::Purchase
     #
     def self.find_driver(*args)
+      find_driver!(*args)
+    rescue NameError
+    end
+
+    def self.find_driver!(*args)
       driver = args.map(&:to_s).map(&:camelize).join('::')
       Conduit::Driver.const_get(driver)
     rescue NameError => error
--- spec/classes/util_spec.rb
+++ spec/classes/util_spec.rb
@@ -7,9 +7,16 @@ describe Conduit::Util do
       driver.should_not be_nil
     end
 
+    it 'should not throw exception for missing driver' do
+      driver = Conduit::Util.find_driver(:missing)
+      driver.should be_nil
+    end
+  end
+
+  describe 'find_driver!' do
     it 'should throw NameError if it driver not found' do
       lambda do
-        Conduit::Util.find_driver(:not_really_there)
+        Conduit::Util.find_driver!(:not_really_there)
       end.should raise_error(NameError)
     end
   end
